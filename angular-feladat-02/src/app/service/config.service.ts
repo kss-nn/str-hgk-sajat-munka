330947f2a131f79a68e7662bba89f58faf1c556d
kss-nn <67142065+kss-nn@users.noreply.github.com>
2021-07-30 22:18:10 +0200

    kezdő állapot + movies adatok

---
+++ angular-feladat-02/src/app/service/config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  apiUrl = `http://localhost:3000/`;
+
+  navigation: {label: string, href: string, role: number}[] = [
+    {label: 'Home', href: '', role: 1},
+    {label: 'Editor', href: '/editor', role: 2},
+    {label: 'Admin', href: '/admin', role: 3}
+  ];
+
+  constructor() { }
+}
